cd2feff5460be57ce9c956e9be0ba69fa84b1bce
Samira Costa <112734365+Samiracostaa@users.noreply.github.com>
2024-10-01 10:55:19 -0300

    agendamento.java

---
+++ agendamento.java
@@ -0,0 +1,55 @@
+//agendamento
+
+class Agendamento {
+  
+  private int idAgendamento;
+  private String horario;
+  private String data;
+  private int idMedico;
+  private int idPaciente;
+  private int idConsultorio;
+  
+  //getters e setters
+  
+}
+
+
+//Consultorio
+
+class Consultorio {
+  
+  private int idConsultorio;
+  private String nomeConsultorio;
+  private String telefone;
+  private String endereco;
+  
+  //getters e setters
+  
+}
+
+//Médico
+
+class Consultorio {
+  
+  private int idConsultorio;
+  private String nomeConsultorio;
+  private String telefone;
+  private String endereco;
+  
+  //getters e setters
+  
+}
+
+//Paciente
+
+class Paciente {
+  
+  private int idPaciente;
+  private String nomePaciente;
+  private String telefone;
+  private String endereco;
+  private Double dataNascimento;
+  
+  //getters e setters
+  
+}
